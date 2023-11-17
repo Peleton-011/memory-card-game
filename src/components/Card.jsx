d3874f9e7e5f4075e36e9204d39c5e90d6910197
Nico Diaz <nicodiazan@gmail.com>
2023-11-17 19:26:40 +0100

    Add basis for card interaction

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-const Card = ({ cardData: { name, images, onClick } }) => {
+const Card = ({ cardData: { name, images }, firstClick, secondClick }) => {
+	let isFirstClick = true;
+
 	return (
 		<div
 			style={{ cursor: "pointer", width: "15rem", height: "25rem" }}
-			onClick={onClick}
+			onClick={(e) => {
+				if (isFirstClick) {
+					firstClick(e);
+					isFirstClick = false;
+				} else {
+					secondClick(e);
+				}
+			}}
 		>
 			<h2>Card</h2>{" "}
 			<img
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ cardList }) => {
+
+    const firstClick = () => {
+        console.log("first click");
+    }
+    
+    const secondClick = () => {
+        console.log("second click");
+    }
+
 	return (
 		<div style={{ display: "flex", flexFlow: "row wrap", gap: "1rem" }}>
 			{cardList.map((card) => {
-				return <Card key={card.id} cardData={card} />;
+				return <Card key={card.id} cardData={card} firstClick={firstClick} secondClick={secondClick} />;
 			})}
 		</div>
 	);
