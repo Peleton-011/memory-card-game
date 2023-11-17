e768b0d24cc9310a37c466d8ebbc4d17aa7068a7
Nico Diaz <nicodiazan@gmail.com>
2023-11-17 09:10:04 +0100

    Add basic fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import DAPI from './APIs/DAPI'
+import { useState, useEffect } from "react";
+import "./App.css";
+import DAPI from "./APIs/DAPI";
 
 function App() {
-  const [name, setName] = useState("")
-
-  useEffect(() => {
-      const fetchData =  () => {
-          try {
-              DAPI.get("10").then((res) => {
-                  console.log(res)
-                  setName(res.data.name)
-              })
-          } catch (error) {
-              console.log(error)
-          }
-      }
-      fetchData()
-  }, [])
-  return (
-    <>
-        {
-            name
-        }
-    </>
-  )
+	const [name, setName] = useState("");
+    
+	useEffect(() => {
+		const fetchData = () => {
+			try {
+				DAPI.get("10").then((res) => {
+					console.log(res);
+					setName(res.data.name);
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
+	return <>{name}</>;
 }
 
-export default App
+export default App;
